b261f792c7ff0384f697d3772099f8bc51815f61
Ren <nsoharab@gmail.com>
2020-11-08 05:21:57 +0200

    improved deep merging of theme files

--- src/Proxima.js
+++ src/Proxima.js
@@ -10,8 +10,31 @@ import global from './global'
 const Proxima = (props) => {
 
     let themeObject = theme;
+
+    const isObject = (item) => {
+        return (item && typeof item === 'object' && !Array.isArray(item));
+    }
+      
+    const mergeDeep = (target, ...sources) => {
+        if (!sources.length) return target;
+        const source = sources.shift();
+      
+        if (isObject(target) && isObject(source)) {
+          for (const key in source) {
+            if (isObject(source[key])) {
+              if (!target[key]) Object.assign(target, { [key]: {} });
+              mergeDeep(target[key], source[key]);
+            } else {
+              Object.assign(target, { [key]: source[key] });
+            }
+          }
+        }
+      
+        return mergeDeep(target, ...sources);
+    }
+
     if(props.theme) {
-        themeObject = {...themeObject, ...props.theme}
+        themeObject = mergeDeep(themeObject, props.theme)
     }
     
     return(
